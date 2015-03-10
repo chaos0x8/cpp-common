0c20c71484a48b25fe625cfd9fbfd29da881d618
chaos0x8 <chaos0x8@users.noreply.github.com>
2015-03-10 08:04:58 +0100

    removed generated file Source/Common.hpp

--- Source/Common.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <Common/Eql.hpp>
-#include <Common/FileUtility.hpp>
-#include <Common/GetCurrentYear.hpp>
-#include <Common/IndexList.hpp>
-#include <Common/StaticAllocator.hpp>
-
