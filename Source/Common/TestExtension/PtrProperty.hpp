c65291e5b298c05228af6589e07b621e338f6f86
chaos0x8 <chaos0x8@users.noreply.github.com>
2016-03-26 09:18:42 +0100

    added matchers PtrField and PtrProperty

---
+++ Source/Common/TestExtension/PtrProperty.hpp
@@ -0,0 +1,117 @@
+/*!
+    \author <https://github.com/chaos0x8>
+    \copyright
+    Copyright (c) 2015, <https://github.com/chaos0x8>
+
+    \copyright
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose with or without fee is hereby granted, provided that the above
+    copyright notice and this permission notice appear in all copies.
+
+    \copyright
+    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
+#pragma once
+
+#include <memory>
+#include <gmock/gmock.h>
+
+namespace Common
+{
+namespace Detail
+{
+
+template <class T> struct UnPtr;
+
+template <class T> struct UnPtr<std::unique_ptr<T>>
+{
+    using type = T;
+    using matcher_type = const std::unique_ptr<T>&;
+};
+
+template <class T> struct UnPtr<std::shared_ptr<T>>
+{
+    using type = T;
+    using matcher_type = std::shared_ptr<T>;
+};
+
+template <class T, class U>
+class PtrFieldMatcher : public testing::MatcherInterface<typename UnPtr<U>::matcher_type>
+{
+public:
+    using C = typename UnPtr<U>::type;
+
+    explicit PtrFieldMatcher(T C::*field, const T& expected)
+        : field(field), expected(expected) { }
+
+    bool MatchAndExplain(typename UnPtr<U>::matcher_type object, testing::MatchResultListener* listener) const override
+    {
+        return (*object).*field == expected;
+    }
+
+    void DescribeTo(std::ostream* os) const override
+    {
+        *os << "field equals " << expected;
+    }
+
+    void DescribeNegationTo(std::ostream* os) const override
+    {
+        *os << "field does not equal " << expected;
+    }
+
+private:
+    T C::*field;
+    T expected;
+};
+
+template <class T, class U>
+class PtrPropertyMatcher : public testing::MatcherInterface<typename UnPtr<U>::matcher_type>
+{
+public:
+    using C = typename UnPtr<U>::type;
+
+    explicit PtrPropertyMatcher(T (C::*property)() const, const T& expected)
+        : property(property), expected(expected) { }
+
+    bool MatchAndExplain(typename UnPtr<U>::matcher_type object, testing::MatchResultListener* listener) const override
+    {
+        return ((*object).*property)() == expected;
+    }
+
+    void DescribeTo(std::ostream* os) const override
+    {
+        *os << "property equals " << expected;
+    }
+
+    void DescribeNegationTo(std::ostream* os) const override
+    {
+        *os << "property does not equal " << expected;
+    }
+
+private:
+    T (C::*property)() const;
+    T expected;
+};
+
+}
+
+template <template <class, class...> class Ptr, class U, class T, class K>
+inline testing::Matcher<typename Detail::UnPtr<Ptr<U>>::matcher_type> PtrField(T U::*field, const K& expected)
+{
+    return MakeMatcher(new Detail::PtrFieldMatcher<T, Ptr<U>>(field, static_cast<T>(expected)));
+}
+
+template <template <class, class...> class Ptr, class U, class T, class K>
+inline testing::Matcher<typename Detail::UnPtr<Ptr<U>>::matcher_type> PtrProperty(T (U::*property)() const, const K& expected)
+{
+    return MakeMatcher(new Detail::PtrPropertyMatcher<T, Ptr<U>>(property, static_cast<T>(expected)));
+}
+
+}
---
+++ Source/Common/TestModules/PtrPropertyTestSuite.cpp
@@ -0,0 +1,126 @@
+/*!
+    \author <https://github.com/chaos0x8>
+    \copyright
+    Copyright (c) 2015, <https://github.com/chaos0x8>
+
+    \copyright
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose with or without fee is hereby granted, provided that the above
+    copyright notice and this permission notice appear in all copies.
+
+    \copyright
+    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
+#include <Common/TestExtension/PtrProperty.hpp>
+#include <Common/TestExtension/MockFunction.hpp>
+#include <gmock/gmock.h>
+
+namespace Common
+{
+namespace UT
+{
+
+using namespace testing;
+
+struct Foo
+{
+    std::string property() const { return _property; }
+
+    void property(const std::string& value) { _property = value; }
+
+    uint32_t field = 0;
+
+private:
+    std::string _property;
+};
+
+class PtrPropertyTestSuite : public Test
+{
+public:
+    Foo* fill(Foo* foo) const
+    {
+        foo->field = FIELD;
+        foo->property(PROPERTY);
+        return foo;
+    }
+
+    template <template <class, class...> class Ptr>
+    void testMatchesCorrectValue()
+    {
+        auto foo = Ptr<Foo>(fill(new Foo));
+
+        ASSERT_THAT(foo, PtrField<Ptr>(&Foo::field, FIELD));
+        ASSERT_THAT(foo, PtrProperty<Ptr>(&Foo::property, PROPERTY));
+    }
+
+    template <template <class, class...> class Ptr>
+    void testFailsWhenValueIsIncorrect()
+    {
+        auto foo = Ptr<Foo>(new Foo);
+
+        ASSERT_THAT(foo, Not(PtrField<Ptr>(&Foo::field, FIELD)));
+        ASSERT_THAT(foo, Not(PtrProperty<Ptr>(&Foo::property, PROPERTY)));
+    }
+
+    template <template <class, class...> class Ptr>
+    void testMatchesValueInExpectCall()
+    {
+        auto foo = Ptr<Foo>(fill(new Foo));
+
+        StrictMock<MockFunction<void (const Ptr<Foo>&)>> mock;
+
+        EXPECT_CALL(mock, Call(AllOf(
+            PtrField<Ptr>(&Foo::field, FIELD), PtrProperty<Ptr>(&Foo::property, PROPERTY))));
+
+        mock.Call(foo);
+    }
+
+    template <template <class, class...> class Ptr>
+    void testMatchesValueInExpectCallViaCopy()
+    {
+        auto foo = Ptr<Foo>(fill(new Foo));
+
+        StrictMock<MockFunction<void (Ptr<Foo>)>> mock;
+
+        EXPECT_CALL(mock, Call(AllOf(
+            PtrField<Ptr>(&Foo::field, FIELD), PtrProperty<Ptr>(&Foo::property, PROPERTY))));
+
+        mock.Call(foo);
+    }
+
+    const uint32_t FIELD = 42;
+    const std::string PROPERTY = "hello";
+};
+
+TEST_F(PtrPropertyTestSuite, matchesCorrectValue)
+{
+    testMatchesCorrectValue<std::unique_ptr>();
+    testMatchesCorrectValue<std::shared_ptr>();
+}
+
+TEST_F(PtrPropertyTestSuite, failsWhenValueIsIncorrect)
+{
+    testFailsWhenValueIsIncorrect<std::unique_ptr>();
+    testFailsWhenValueIsIncorrect<std::shared_ptr>();
+}
+
+TEST_F(PtrPropertyTestSuite, matchesValueInExpectCall)
+{
+    testMatchesValueInExpectCall<std::unique_ptr>();
+    testMatchesValueInExpectCall<std::shared_ptr>();
+}
+
+TEST_F(PtrPropertyTestSuite, matchesValueInExpectCallViaCopy)
+{
+    testMatchesValueInExpectCallViaCopy<std::shared_ptr>();
+}
+
+}
+}
