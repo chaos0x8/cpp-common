b1cf8338ccd1106865324298e415bbf46254d5ba
chaos0x8 <chaos0x8@users.noreply.github.com>
2016-05-12 02:06:33 +0200

    refactored rakefile

--- rakefile.rb
+++ rakefile.rb
@@ -18,237 +18,142 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
+require 'date'
+require 'pathname'
+
 def web_require url
     system "wget #{url}" unless File.exist?(File.basename(url))
     require_relative File.basename(url)
 end
 
-web_require "https://raw.github.com/chaos0x8/rake-builder/master/RakeBuilder.rb"
+web_require 'https://raw.github.com/chaos0x8/rake-builder/master/lib/RakeBuilder.rb'
 
-if ARGV.index("nodebug").nil?
-    FLAGS = [ "--std=c++14", "-g" ]
-else
-    FLAGS = [ "--std=c++14", "-O3", "-s", "-DNDEBUG" ]
+flags = [ '--std=c++14', '-O3', '-s', '-DNDEBUG' ]
 
-    task :nodebug
-end
+license = File.open('LICENSE', 'r') { |f|
+    result = Array.new
 
-INCLUDES = [ "Source" ]
-generatedFiles = [ "Source/Generated/CacheLineSize.hpp" ]
+    result << "/*!\n"
+    f.each_line { |line|
+        line = line.gsub('#{year}', DateTime::now.year.to_s)
 
-def authorNotice
-    f = File.open "LICENSE", "r"
-    data = f.read
-    f.close
+        result << " *  #{line}".rstrip + "\n"
+    }
+    result << " */\n\n"
 
-    text = "/*!\n"
-    data.split("\n").each do |line|
-        if line.strip == ""
-            text += "\n"
-        else
-            text += "    #{line}\n"
-        end
-    end
+    result.join('')
+}
 
-    "#{text}*/\n\n"
-end
+generated = Array.new
 
-def generateDirectoryInclude *directories
-    existing = Dir.glob("Source/*").select { |d| File.directory?(d) }
-    existing = existing.collect { |d| File.basename(d) }
+generated << Generated.new { |t|
+    t.name = "Source/Generated/CacheLineSize.hpp"
+    t.code = Proc.new {
+        puts "Generating '#{t.name}'..."
 
-    (directories + (existing - directories)).each do |directory|
-        yield "Source/#{directory}.hpp"
+        cacheLineSize = `getconf LEVEL1_DCACHE_LINESIZE`.chomp
 
-        file "Source/#{directory}.hpp" => "rakefile.rb" do |fileName|
-            raise RuntimeError.new("Directory doesn't exist: 'Source/#{directory}'") unless File.directory?("Source/#{directory}")
-            puts "Generating '#{fileName}'..."
-
-            files = FileList["Source/#{directory}/*.hpp"]
-
-            f = File.open(fileName.to_s, "w")
-            f.write authorNotice
+        File.open(t.name, 'w') { |f|
+            f.write license
             f.write "#pragma once\n"
             f.write "\n"
-            files.each { |x| f.write "#include <#{directory}/#{File.basename(x)}>\n" }
+            f.write "#define CACHE_LINE_SIZE (#{cacheLineSize}ul)\n"
             f.write "\n"
-            f.close
+        }
+    }
+    t.description = "Generates '#{t.name}'"
+}
+
+tmp = [ 'Source/Generated', 'Source/Common/Exceptions' ] + Dir['Source/*'].select{ |x| File.directory?(x) }
+tmp.uniq.each { |dir|
+    generated << Generated.new { |t|
+        t.name = "#{dir}.hpp"
+        t.code = Proc.new {
+            puts "Generating '#{t.name}'..."
+
+            headers = Dir["#{dir}/*.hpp"].collect { |h|
+                Pathname.new(h).relative_path_from(Pathname.new('Source'))
+            }
+
+            File.open(t.name, 'w') { |f|
+                f.write license
+                f.write "#pragma once\n"
+                f.write "\n"
+                f.write headers.collect{ |h| "#include <#{h}>\n" }.join('')
+                f.write "\n"
+            }
+        }
+        t.description = "Generates '#{t.name}'"
+    }
+}
+
+librariesCommon = Array.new
+
+librariesCommon << Library.new { |t|
+    t.name = 'lib/libcommon.a'
+    t.includes = [ 'Source' ]
+    t.flags = [ flags ]
+    t.files = generated
+    t.sources = Dir['Source/Common/**/*.cpp'] - Dir['Source/Common/TestModules/*.cpp']
+    t.description = "Builds library '#{t.name}'"
+}
+
+libraries = Array.new
+
+tmp = Dir['Source/*'].select{ |x| File.directory?(x) } - [ 'Source/Common' ]
+tmp.each { |dir|
+    sources = Dir["#{dir}/**/*.cpp"] - Dir["#{dir}/**/TestModules/*.cpp"]
+
+    libraries << Library.new { |t|
+        t.name = "lib/libcommon#{File.basename(dir)}.a"
+        t.includes = [ 'Source' ]
+        t.flags = [ flags ]
+        t.files = generated
+        t.sources = sources
+        t.libs = librariesCommon
+        t.description = "Builds library '#{t.name}'"
+
+        case t.name
+        when 'lib/libcommonGtkmm.a'
+            t.libs << Pkg['gtkmm-3.0']
+        when 'lib/libcommonEmbededRuby.a'
+            t.libs << Pkg['ruby-1.9']
+        when 'lib/libcommonSqLite.a'
+            t.libs << Pkg['sqlite3']
+        when 'lib/libcommonGL.a'
+            t.libs << Pkg['glew']
         end
-    end
-end
+    } if sources.size > 0
+}
 
-directory "Source/Generated"
-file "Source/Generated/CacheLineSize.hpp" => [ "Source/Generated", "rakefile.rb" ] do |fileName|
-    CACHE_LINE_SIZE = `getconf LEVEL1_DCACHE_LINESIZE`.chomp
+uts = Array.new
 
-    puts "Generating '#{fileName}'..."
+Dir['Source/*'].select{ |x| File.directory?(x) }.each { |dir|
+    sources = Dir["#{dir}/**/TestModules/*.cpp"]
 
-    f = File.open(fileName.to_s, "w")
-    f.write authorNotice
-    f.write "#pragma once\n"
-    f.write "\n"
-    f.write "#define CACHE_LINE_SIZE (#{CACHE_LINE_SIZE}ul)\n"
-    f.write "\n"
-    f.close
-end
+    uts << Executable.new { |t|
+        t.name = "bin/#{File.basename(dir)}-ut"
+        t.includes = [ 'Source' ]
+        t.flags = [ flags, '-pthread' ]
+        t.files = generated
+        t.sources = sources
+        t.libs = [ '-lgtest', '-lgmock' ]
+        t.libs += libraries.select { |l| l.name == "lib/libcommon#{File.basename(dir)}.a" }
+        t.libs << librariesCommon
+        t.description = "Builds ut target '#{t.name}'"
+    } if sources.size > 0
+}
 
-generateDirectoryInclude 'Generated', 'Common/Exceptions' do |fileName|
-    generatedFiles.push fileName
-end
+desc 'Builds all generated files'
+multitask(:generated => generated.collect{|x|x.name})
 
-Library.new do |t|
-    t.name = "lib/libcommon.a"
-    t.includes = INCLUDES
-    t.flags = FLAGS
-    t.files = FileList[ "Source/Common/*.cpp", "Source/Common/Exceptions/*.cpp" ]
-    t.dependencies = generatedFiles
-end
+desc 'Builds all libraries'
+multitask(:libraries => libraries.collect{|x|x.name})
 
-Library.new do |t|
-    t.name = "lib/libcommonSqLite.a"
-    t.includes = INCLUDES
-    t.flags = FLAGS
-    t.files = FileList[ "Source/SqLite/*.cpp" ]
-    t.dependencies = generatedFiles
-end
+desc 'Builds everything & runs uts'
+multitask(:default => uts.collect{|x|x.name}) {
+    uts.each { |x|
+        sh x.name
+    }
+}
 
-Library.new do |t|
-    t.name = "lib/libcommonGtkmm.a"
-    t.includes = INCLUDES
-    t.flags = FLAGS
-    t.files = FileList[ "Source/Gtkmm/*.cpp" ]
-    t.libs = [ Pkg.new("gtkmm-3.0") ]
-    t.dependencies = generatedFiles
-end
-
-Library.new do |t|
-    t.name = "lib/libcommonParallel.a"
-    t.includes = INCLUDES
-    t.flags = FLAGS
-    t.files = FileList[ "Source/Parallel/*.cpp" ]
-    t.dependencies = generatedFiles
-end
-
-Library.new do |t|
-    t.name = "lib/libcommonNetwork.a"
-    t.includes = INCLUDES
-    t.flags = FLAGS
-    t.files = FileList[ "Source/Network/*.cpp", "Source/Network/Detail/*.cpp" ]
-    t.dependencies = generatedFiles
-end
-
-Library.new do |t|
-    t.name = "lib/libcommonGL.a"
-    t.includes = INCLUDES
-    t.flags = FLAGS
-    t.files = FileList[ "Source/GL/*.cpp", "Source/GL/Detail/*.cpp" ]
-    t.dependencies = generatedFiles
-end
-
-Library.new do |t|
-    t.name = "lib/libcommonSfml.a"
-    t.includes = INCLUDES
-    t.flags = FLAGS
-    t.files = FileList[ "Source/Sfml/*.cpp", "Source/Sfml/Detail/*.cpp" ]
-    t.dependencies = generatedFiles
-end
-
-Library.new do |t|
-    t.name = 'lib/libcommonNCurses.a'
-    t.includes = INCLUDES
-    t.flags = FLAGS
-    t.files = FileList[ 'Source/NCurses/*.cpp' ]
-    t.libs = [ Pkg.new('ncurses') ]
-    t.dependencies = generatedFiles
-end
-
-Library.new do |t|
-    t.name = 'lib/libcommonEmbededRuby.a'
-    t.includes = INCLUDES
-    t.flags = FLAGS
-    t.files = FileList[ 'Source/EmbededRuby/*.cpp', 'Source/EmbededRuby/Detail/*.cpp' ]
-    t.libs = [ Pkg.new('ruby-1.9') ]
-    t.dependencies = generatedFiles
-end
-
-Application.new do |t|
-    t.name = "bin/common-ut"
-    t.includes = INCLUDES
-    t.flags = FLAGS + [ '-DUT_MODE=1' ]
-    t.files = FileList[ "Source/Common/TestModules/*.cpp" ]
-    t.dependencies = [ "lib/libcommon.a" ] + generatedFiles
-    t.libs = [ "-lgtest", "-lgmock", "-Llib", "-lcommon", "-lpthread", "-lrt" ]
-end
-
-Application.new do |t|
-    t.name = "bin/commonTraits-ut"
-    t.includes = INCLUDES
-    t.flags = FLAGS
-    t.files = FileList[ "Source/Traits/TestModules/*.cpp" ]
-    t.libs = [ "-lgtest", "-lgmock", "-lpthread" ]
-end
-
-Application.new do |t|
-    t.name = "bin/commonParallel-ut"
-    t.includes = INCLUDES
-    t.flags = FLAGS
-    t.files = FileList[ "Source/Parallel/TestModules/*.cpp" ]
-    t.dependencies = [ "lib/libcommonParallel.a" ] + generatedFiles
-    t.libs = [ "-lgtest", "-lgmock", "-Llib", "-lcommonParallel", "-lpthread" ]
-end
-
-Application.new do |t|
-    t.name = "bin/commonSqLite-ut"
-    t.includes = INCLUDES
-    t.flags = FLAGS
-    t.files = FileList[ "Source/SqLite/TestModules/*.cpp" ]
-    t.dependencies = [ "lib/libcommonSqLite.a" ] + generatedFiles
-    t.libs = [ "-lgtest", "-lgmock", "-Llib", "-lcommonSqLite", "-lpthread", Pkg.new('sqlite3') ]
-end
-
-Application.new do |t|
-    t.name = "bin/commonNetwork-ut"
-    t.includes = INCLUDES
-    t.flags = FLAGS
-    t.files = FileList[ "Source/Network/TestModules/*.cpp" ]
-    t.dependencies = [ "lib/libcommon.a", "lib/libcommonNetwork.a" ] + generatedFiles
-    t.libs = [ "-lgtest", "-lgmock", "-Llib", "-lcommonNetwork", "-lcommon", "-lpthread" ]
-end
-
-Application.new do |t|
-    t.name = "bin/commonGL-ut"
-    t.includes = INCLUDES
-    t.flags = FLAGS
-    t.files = FileList[ "Source/GL/**TestModules/*.cpp" ]
-    t.dependencies = [ "lib/libcommon.a", "lib/libcommonGL.a" ] + generatedFiles
-    t.libs = [ "-lgtest", "-lgmock", "-Llib", "-lcommonGL", "-lcommon", "-lpthread", Pkg.new('glew') ]
-end
-
-Application.new do |t|
-    t.name = 'bin/commonEmbededRuby-ut'
-    t.includes = INCLUDES
-    t.flags = FLAGS
-    t.files = FileList[ 'Source/EmbededRuby/**TestModules/*.cpp' ]
-    t.dependencies = [ 'lib/libcommon.a', 'lib/libcommonEmbededRuby.a' ] + generatedFiles
-    t.libs = [ '-lgtest', '-lgmock', '-Llib', '-lcommonEmbededRuby', '-lcommon', '-lpthread', Pkg.new('ruby-1.9') ]
-end
-
-utList = { :commonUT => 'bin/common-ut', :commonTraitsUT => 'bin/commonTraits-ut', :commonSqLiteUT => 'bin/commonSqLite-ut', :commonParallelUT => 'bin/commonParallel-ut',
-           :commonNetworkUT => 'bin/commonNetwork-ut', :commonGlUT => 'bin/commonGL-ut', :commonEmbededRubyUT => 'bin/commonEmbededRuby-ut' }
-utList.each do |tar, res|
-    desc "Compiles and runs ut fule #{res}"
-    multitask tar => res do
-        sh res
-    end
-end
-
-subProjects = Dir['*/**/rakefile.rb']
-
-desc 'Compile and run all uts'
-task :ut => utList.keys
-
-desc 'Compile all libs'
-multitask :libs => Target.tasks(/\.a$/)
-
-desc 'Compile all libs and run all uts'
-task :default => [ :libs, :ut ]
