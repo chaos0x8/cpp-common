39425f403d521374231f48b89fc97c54744c5415
chaos.0x8 <krzychu.0x8@gmail.com>
2020-06-13 13:46:50 +0200

    added operator==(NativeHandler) for socket class

--- Test/Network/TcpIpSocketsTestSuite.cpp
+++ Test/Network/TcpIpSocketsTestSuite.cpp
@@ -24,95 +24,98 @@
 #include <future>
 #include <gmock/gmock.h>
 
-namespace Common {
-  namespace Network {
-    namespace UT {
+namespace Common::Network {
+  using namespace testing;
 
-      using namespace testing;
+  class TcpIpSocketsTestSuite : public Test {
+  public:
+    TcpIpSocketsTestSuite() {
+      acceptResult =
+        std::async(std::launch::async, [&] { return listener.accept(); });
+    }
 
-      class TcpIpSocketsTestSuite : public Test {
-      public:
-        TcpIpSocketsTestSuite() {
-          acceptResult =
-            std::async(std::launch::async, [&] { return listener.accept(); });
-        }
+    TcpIpServer listener{LOCAL_HOST, PORT};
+    std::future<TcpIpClient> acceptResult;
 
-        TcpIpServer listener{LOCAL_HOST, PORT};
-        std::future<TcpIpClient> acceptResult;
+    static const std::string LOCAL_HOST;
+    static const std::string PORT;
+  };
 
-        static const std::string LOCAL_HOST;
-        static const std::string PORT;
-      };
+  const std::string TcpIpSocketsTestSuite::LOCAL_HOST = "127.0.0.1";
+  const std::string TcpIpSocketsTestSuite::PORT = "3042";
 
-      const std::string TcpIpSocketsTestSuite::LOCAL_HOST = "127.0.0.1";
-      const std::string TcpIpSocketsTestSuite::PORT = "3042";
+  TEST_F(TcpIpSocketsTestSuite, sendReceive) {
+    TcpIpClient clientClientSide = TcpIpClient{LOCAL_HOST, PORT};
+    clientClientSide.send("Hello world");
 
-      TEST_F(TcpIpSocketsTestSuite, sendReceive) {
-        TcpIpClient clientClientSide = TcpIpClient{LOCAL_HOST, PORT};
-        clientClientSide.send("Hello world");
+    TcpIpClient clientServerSide = acceptResult.get();
+    ASSERT_THAT(clientServerSide.receive(), Eq("Hello world"));
+  }
 
-        TcpIpClient clientServerSide = acceptResult.get();
-        ASSERT_THAT(clientServerSide.receive(), Eq("Hello world"));
+  TEST_F(TcpIpSocketsTestSuite, sendRecvStringWithSize) {
+    TcpIpClient clientClientSide = TcpIpClient{LOCAL_HOST, PORT};
+    clientClientSide.send("Hello world");
+
+    TcpIpClient clientServerSide = acceptResult.get();
+    ASSERT_THAT(clientServerSide.receive(3), Eq("Hel"));
+  }
+
+  TEST_F(TcpIpSocketsTestSuite, shoutdownClient) {
+    TcpIpClient clientClientSide = TcpIpClient{LOCAL_HOST, PORT};
+    TcpIpClient clientServerSide = acceptResult.get();
+
+    clientServerSide.shutdown();
+
+    ASSERT_THAT(clientClientSide.receive(), IsEmpty());
+    ASSERT_THAT(clientServerSide.receive(), IsEmpty());
+  }
+
+  TEST_F(TcpIpSocketsTestSuite, shutdownListenerThrowsException) {
+    TcpIpClient clientClientSide = TcpIpClient{LOCAL_HOST, PORT};
+    TcpIpClient clientServerSide = acceptResult.get();
+
+    ASSERT_THROW(listener.shutdown(), Exceptions::SystemError);
+  }
+
+  TEST_F(TcpIpSocketsTestSuite, shouldCompareWithNativeHandler) {
+    TcpIpClient clientClientSide = TcpIpClient{LOCAL_HOST, PORT};
+    TcpIpClient clientServerSide = acceptResult.get();
+
+    ASSERT_THAT(clientClientSide, Eq(clientClientSide.getNativeHandler()));
+    ASSERT_THAT(clientClientSide, Ne(clientServerSide.getNativeHandler()));
+  }
+
+  struct TcpIpSockets_rawMessages_TestSuite : public TcpIpSocketsTestSuite {
+    struct Message {
+      bool operator==(const Message& other) const {
+        return x == other.x and y == other.y;
       }
 
-      TEST_F(TcpIpSocketsTestSuite, sendRecvStringWithSize) {
-        TcpIpClient clientClientSide = TcpIpClient{LOCAL_HOST, PORT};
-        clientClientSide.send("Hello world");
+      int x;
+      int y;
+    };
 
-        TcpIpClient clientServerSide = acceptResult.get();
-        ASSERT_THAT(clientServerSide.receive(3), Eq("Hel"));
-      }
+    Message msg{7, 42};
+    Message act{};
+  };
 
-      TEST_F(TcpIpSocketsTestSuite, shoutdownClient) {
-        TcpIpClient clientClientSide = TcpIpClient{LOCAL_HOST, PORT};
-        TcpIpClient clientServerSide = acceptResult.get();
+  TEST_F(TcpIpSockets_rawMessages_TestSuite, sendReceive) {
+    TcpIpClient clientClientSide = TcpIpClient{LOCAL_HOST, PORT};
+    TcpIpClient clientServerSide = acceptResult.get();
 
-        clientServerSide.shutdown();
+    clientClientSide.send(&msg, sizeof(msg));
 
-        ASSERT_THAT(clientClientSide.receive(), IsEmpty());
-        ASSERT_THAT(clientServerSide.receive(), IsEmpty());
-      }
+    ASSERT_THAT(clientServerSide.receive(&act, sizeof(act)), Eq(true));
+    ASSERT_THAT(act, Eq(msg));
+  }
 
-      TEST_F(TcpIpSocketsTestSuite, shutdownListenerThrowsException) {
-        TcpIpClient clientClientSide = TcpIpClient{LOCAL_HOST, PORT};
-        TcpIpClient clientServerSide = acceptResult.get();
+  TEST_F(TcpIpSockets_rawMessages_TestSuite, shoutdownClient) {
+    TcpIpClient clientClientSide = TcpIpClient{LOCAL_HOST, PORT};
+    TcpIpClient clientServerSide = acceptResult.get();
 
-        ASSERT_THROW(listener.shutdown(), Exceptions::SystemError);
-      }
+    clientServerSide.shutdown();
 
-      struct TcpIpSockets_rawMessages_TestSuite : public TcpIpSocketsTestSuite {
-        struct Message {
-          bool operator==(const Message& other) const {
-            return x == other.x and y == other.y;
-          }
-
-          int x;
-          int y;
-        };
-
-        Message msg{7, 42};
-        Message act{};
-      };
-
-      TEST_F(TcpIpSockets_rawMessages_TestSuite, sendReceive) {
-        TcpIpClient clientClientSide = TcpIpClient{LOCAL_HOST, PORT};
-        TcpIpClient clientServerSide = acceptResult.get();
-
-        clientClientSide.send(&msg, sizeof(msg));
-
-        ASSERT_THAT(clientServerSide.receive(&act, sizeof(act)), Eq(true));
-        ASSERT_THAT(act, Eq(msg));
-      }
-
-      TEST_F(TcpIpSockets_rawMessages_TestSuite, shoutdownClient) {
-        TcpIpClient clientClientSide = TcpIpClient{LOCAL_HOST, PORT};
-        TcpIpClient clientServerSide = acceptResult.get();
-
-        clientServerSide.shutdown();
-
-        ASSERT_THAT(clientClientSide.receive(&act, sizeof(act)), Eq(false));
-        ASSERT_THAT(clientServerSide.receive(&act, sizeof(act)), Eq(false));
-      }
-    } // namespace UT
-  }   // namespace Network
-} // namespace Common
+    ASSERT_THAT(clientClientSide.receive(&act, sizeof(act)), Eq(false));
+    ASSERT_THAT(clientServerSide.receive(&act, sizeof(act)), Eq(false));
+  }
+} // namespace Common::Network
